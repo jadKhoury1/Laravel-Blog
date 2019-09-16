8b2ae760e139a16b6d9262b929c8dff2494d7ccf
Jad Khoury <jad.khoury@foo-solutions.com>
2019-09-16 23:41:30 +0300

    Added Base classes to the project

---
+++ app/Base/BaseController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Base;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as LaravelBaseController;
+
+
+class BaseController extends LaravelBaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Stores Error Messages
+     *
+     * @var string
+     */
+    protected $errorMessage;
+
+    /**
+     * Array that stores validation rules
+     *
+     * @var array
+     */
+    protected $rules;
+
+
+    /**
+     * Stores the Request Object
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * Stores the response object
+     *
+     * @var BaseResponse
+     */
+    protected $response;
+
+
+    public function __construct(Request $request, BaseResponse $response)
+    {
+        $this->request  = $request;
+        $this->response = $response;
+    }
+
+    /**
+     * Apply validation rules on request params
+     *
+     * @return bool
+     */
+    protected function makeValidation()
+    {
+        $validation = Validator::make($this->request->all(), $this->rules, $this->setCustomErrorMessages());
+
+        if ($validation->fails()) {
+            $this->errorMessage = $validation->errors()->first();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Set Custom Error Messages for validation rules
+     *
+     * @return array
+     */
+    protected function setCustomErrorMessages()
+    {
+        return [];
+    }
+}
