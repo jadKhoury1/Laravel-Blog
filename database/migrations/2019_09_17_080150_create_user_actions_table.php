6521436df01c8433ed56b30539a957a51113d8a8
Jad Khoury <jad.khoury@foo-solutions.com>
2019-09-17 11:14:07 +0300

    Added migration that creates 'user_actions' table

---
+++ database/migrations/2019_09_17_080150_create_user_actions_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_actions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->enum('action', ['ADD', 'EDIT', 'DELETE']);
+            $table->string('model');
+            $table->integer('object_id')->unsigned();
+            $table->text('data')->nullable();
+            $table->integer('status')->default(0);
+            $table->bigInteger('action_taken_by')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('CASCADE')
+                  ->onUpdate('CASCADE');
+
+            $table->foreign('action_taken_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('SET NULL')
+                ->onUpdate('SET NULL');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_actions');
+    }
+}
